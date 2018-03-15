9ba18be378ebf1ed51c5c0cf137de5c4fa825c6f
V.Kharitonovitch <morainrallez@gmail.com>
2018-03-15 22:18:54 +0300

    yor-are-awesome/feature: 5/10 functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 // DO WHATEVER YOU WANT HERE
-
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+let n = 0;
+const createEnumerableProperty = (propertyName) => { return propertyName;};
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName,{
+        enumerable:false,
+        value: 'value'
+    });
+    return propertyName;
+};
+const createProtoMagicObject = () => {return Function};
+const incrementor = () => {
+    n++;
+    return n;
+};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = (object) => {
+    let something = new Boolean(object);
+    return something;
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+    const compareFunction = (a,b) =>{
+        return a-b;
+        }
+  return  arr.sort(compareFunction);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
